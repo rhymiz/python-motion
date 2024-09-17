bb16120b1cce09a950f4b02ffe39522c14da855b
daquintero <darioaquintero@gmail.com>
2024-09-17 14:25:12 +0200

    Leaving for today

--- tests/test_projects_list.py
+++ tests/test_projects_list.py
@@ -1,1 +1,18 @@
+# +
+import os
+from motion import Motion, MotionResponse
+
+# Initialize the Motion client with the API token
+motion = Motion(os.getenv("MOTION_API_TOKEN"))
+
+# Fetch tasks
+response = motion.tasks.list()
+# -
+
+dir(motion)
+
+workspaces = motion.workspaces.list()
+
+workspaces.json()
+
 
